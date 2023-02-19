fbf5edcec05107fbaa318f8f4ab4558f8520cc3a
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-18 21:27:09 -0500

    added basic content for the aboutme section

---
+++ src/pages/aboutMe/career.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Career(props) {
+	return (
+		<p>
+			I graduated from Purdue University with a bachelor's degree in
+			chemical engineering. I started with ADM in June of 2017 where I
+			started as a production engineer. Within one and half years, I was
+			promoted to the role of a process engineer. I left ADM to pursue a
+			role within operations management as a Process Lead at Ingredion.
+			After one year in that role, I was promoted to Department Manager of
+			the Corn Co-products department. A little over a year later, I'm
+			transitioning to a senior engineer role. While I've learned and
+			accomplished so much in the manufacturing role, I've realized that I
+			enjoy computer programming so much more than what I learned with my
+			original degree. Transitioning to a senior engineer will allow me to
+			continue to provide value to Ingredion while I develop a software
+			engineering skillset.
+		</p>
+	);
+}
---
+++ src/pages/aboutMe/education.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Education(props) {
+	return (
+		<p>
+			At Purdue, I learned Java, Python, C, and MATLAB. On my own time,
+			I've focused on C++ and some python, but I wanted to explore web
+			development. I joined the University of Wisconsin Web Development
+			Bootcamp to harness my previous knowledge and experience and apply
+			it to the infrastructure of the internet. My personal website will
+			act as an interactive host to my featured projects ranging from
+			website, fullstack applications, desktop applications, and mobile
+			apps.
+		</p>
+	);
+}
