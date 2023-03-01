bee793e09404501a9d9044c16ea461cae9ef2492
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-28 19:41:30 -0500

    added more files/folder structures

---
+++ server/utils/auth.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+dotenv.config();
+import jwt from "jsonwebtoken";
+
+// TODO: Refactor as my own work and for improved performance/functionality!
+
+// set token secret and expiration date
+const secret = process.env.SERVER_SECRET;
+const expiration = "2h";
+
+export function signToken({ username, email, _id }) {
+	const payload = { username, email, _id };
+	return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
+
+// function for our authenticated routes
+export function authMiddleware({ req, res }) {
+	// allows token to be sent via body, query, or header
+	let token = req.body.token || req.query.token || req.headers.authorization;
+	// ["Bearer", "<tokenvalue>"]
+	if (req.headers.authorization) {
+		token = token.split(" ").pop().trim();
+	}
+
+	if (!token) {
+		return req; // Return request unmodified
+	}
+
+	// verify token and get user data out of it
+	try {
+		const { data } = jwt.verify(token, secret, { maxAge: expiration });
+		req.user = data; // Added to context of each resolver
+		return req;
+	} catch (err) {
+		console.error("Invalid token", err);
+	}
+}
