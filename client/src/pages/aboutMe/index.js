5e90502765e51c50f46c784fc087dd028eec72a0
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-02-22 00:10:24 -0500

    added headings to each section

--- client/src/pages/aboutMe/index.js
+++ client/src/pages/aboutMe/index.js
@@ -6,8 +6,12 @@ import Personal from "./personal";
 export default function AboutMe(props) {
 	return (
 		<>
+			<h3>Introduction:</h3>
+			<h4>Education</h4>
 			<Education />
+			<h4>Career</h4>
 			<Career />
+			<h4>Personal</h4>
 			<Personal />
 		</>
 	);
