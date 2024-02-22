9af79ba9dce8a933206c04cacfeee4e45ac1f272
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-02-22 00:01:49 -0500

    testing styles on blog post

--- client/src/pages/blog/index.js
+++ client/src/pages/blog/index.js
@@ -1,3 +1,19 @@
+function post(props) {
+	return (
+		<>
+			<h3>My Blog</h3>
+			<div
+				class="Post"
+				style={{ border: "solid 5px black", width: "fit-content" }}
+			>
+				<div>Title of Post</div>
+				<div>Content of Post</div>
+				<div>Footer of Post</div>
+			</div>
+		</>
+	);
+}
+
 export default function Blog(props) {
-	return <>My Blog</>;
+	return post(props);
 }
