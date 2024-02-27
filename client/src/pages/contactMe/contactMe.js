3176f7ae53903afed2768bbcf3f70906fbfabc89
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-02-27 16:59:46 -0500

    Updated 'required' styled components

--- client/src/pages/contactMe/contactMe.js
+++ client/src/pages/contactMe/contactMe.js
@@ -14,6 +14,7 @@ export default function ContactForm(props) {
 			<input
 				type="text"
 				name="name"
+				id="contact-name"
 			/>
 			<br />
 			<label>
@@ -22,27 +23,37 @@ export default function ContactForm(props) {
 			<input
 				type="email"
 				name="email"
+				id="contact-email"
 			/>
 			<br />
 			<label>Phone Number: </label>{" "}
 			<input
 				type="tel"
 				name="phone"
+				id="contact-phone"
 			/>
 			<br />
 			<label>Business/Organization: </label>{" "}
 			<input
 				type="text"
 				name="business"
+				id="contact-business"
 			/>
 			<br />
 			<label>
 				Reason for Contact (<Required>required</Required>):
 			</label>{" "}
-			<input
-				type="dropdown"
+			<select
 				name="reason"
-			/>
+				id="contat-reason"
+			>
+				<option value="general">General Inquiry</option>
+				<option value="career">Career Opportunity</option>
+				<option value="business">Business Inquiry</option>
+				<option value="personal">Personal Inquiry</option>
+				<option value="project">Project Inquiry</option>
+				<option value="other">Other</option>
+			</select>
 			<br />
 		</>
 	);
