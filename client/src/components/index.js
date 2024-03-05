1bbd6ee88e4da52073eb34a720ead02ce12788d5
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-03-04 23:14:53 -0500

    added components for each part of the resume

---
+++ client/src/components/index.js
@@ -0,0 +1,21 @@
+import Footer from "./footer";
+import Header from "./header";
+import ProjectContainer from "./projectContainer";
+import Summary from "./summary";
+import TechnicalSkills from "./technicalSkills";
+import EducationCertifications from "./educationCertification";
+import Experience from "./experience";
+import Navigation from "./navbar";
+import NavbarLink from "./navbarLink";
+
+export {
+	Footer,
+	Header,
+	ProjectContainer,
+	Summary,
+	TechnicalSkills,
+	EducationCertifications,
+	Experience,
+	Navigation,
+	NavbarLink,
+};
---
+++ client/src/components/summary.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Summary(props) {
+	//TODO: Change into a variable to pull from database or some other source for easier updates
+	return (
+		<>
+			<h3>Summary</h3>
+			{
+				"Senior Engineer and Software Developer with more than 6 years of experience in project management, strategic planning, and process optimization. Background in establishing standard operating procedures, leading cross-functional teams, and generating performance reports. Key strengths include data analysis, creative problem solving, and learning new systems."
+			}
+		</>
+	);
+}
